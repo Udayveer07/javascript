699ceac32071753846c0df77b50111b14fda2cdd
Udayveer Singh <112947360+Udayveer07@users.noreply.github.com>
2024-07-27 22:11:12 +0000

    operators

--- 01-basics/03-conversionOperations.js
+++ 01-basics/03-conversionOperations.js
@@ -27,3 +27,43 @@ let stringNumber = String(someNumber)
 
 console.log(stringNumber);
 console.log(typeof stringNumber);
+
+
+// *************************** Operations **************************************
+
+let value = 3
+let negValue = -value
+
+// console.log(negValue);
+
+console.log(2+2); // addition
+console.log(2-2); // subtract
+console.log(2*2); // multiple
+console.log(2**3); // power
+console.log(2/3); // divide
+console.log(2%3); // remainder
+
+
+let str1 ="hello"
+let str2 = " uday"
+
+let str3 =str1 + str2
+console.log(str3);
+
+console.log("1"+ 2);
+console.log(1 + "2");
+
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+
+console.log(+true);
+console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+gameCounter++;   // pre & post increment & decrement
+
+console.log(gameCounter);
