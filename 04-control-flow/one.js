68cc5a63bb154a08657535e085d05e5c6c4f61d2
Udayveer Singh <112947360+Udayveer07@users.noreply.github.com>
2024-08-10 19:53:20 +0000

    if else statement

---
+++ 04-control-flow/one.js
@@ -0,0 +1,59 @@
+// if
+
+const isUserLogged = true
+const temp = 41
+
+if (temp > 50) {
+    console.log("less than 50");
+}
+else{
+    console.log("more than 50");
+}
+
+console.log("Always Executed");
+
+if (2 === "2") {
+    console.log("Not Found");
+}
+
+// <, >, <=, >=, ==, !=, ===, !==  types of operators in conditions 
+
+const score = 200
+
+if (score > 100) {
+    const power = "fly"
+    console.log(`Power of User is ${power}`);   
+}
+
+// console.log(`Power of User is ${power}`);   
+
+
+const balance = 1000
+
+if (balance < 500) {
+    console.log("less than 500");
+}
+else if (balance < 750) {
+    console.log("less than 750");
+}
+else if (balance < 900){
+    console.log("less than 900");
+}
+else{
+    console.log("less than 1200");
+}
+
+const userLoggedIn = true
+const debitCard = true
+const loggedInFromEmail = false
+const loggedInFromGoogle = true
+
+
+if (userLoggedIn && debitCard) {  // in && operator all cond should be true
+    console.log("Allowed to buy this course");
+}
+
+if(loggedInFromEmail || loggedInFromGoogle) { // in || operator any one cond should be true
+    console.log("User is Logged IN")
+}
+
