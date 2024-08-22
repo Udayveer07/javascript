59c7c4cd7c854edf5dbdf62a8cfc092a2a19f842
Udayveer Singh <112947360+Udayveer07@users.noreply.github.com>
2024-08-22 12:42:01 +0000

    while & do while loop

--- 05-iterations/two.js
+++ 05-iterations/two.js
@@ -1,5 +1,25 @@
 // while loop
 
-while (condition) {
-    
+let index = 0
+while (index <= 10) {
+    console.log(`Value of index is ${index}`);
+    index = index + 2;
 }
+
+
+let myArray = ['Ironman', 'Batman', 'Superman', 'Hulk']
+
+let arr = 0 
+while (arr < myArray.length) {
+    console.log(`The Hero is ${myArray[arr]}`);
+    arr = arr + 1
+}
+
+
+let score = 1
+
+ do {
+    console.log(`Score is ${score}`);
+    score++
+    
+ } while (score < 10);
